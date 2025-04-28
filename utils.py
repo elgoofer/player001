9b70cde3ceafd26f26416743748e8b0edc48724a
elgoofer <elgoofer@gmail.com>
2025-04-28 17:22:30 -0300

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-# utils.py
-
-def formatear_tiempo(segundos):
-    # Función para convertir segundos a formato "minutos:segundos"
-    minutos = int(segundos // 60)
-    seg = int(segundos % 60)
-    return f"{minutos}:{seg:02d}"
