ec024f5839e28dd6b79072d1136775d0e2d9d852
elgoofer <elgoofer@gmail.com>
2025-04-28 17:21:13 -0300

    Delete info_api.py

--- info_api.py
+++
@@ -1,6 +0,0 @@
-# info_api.py
-
-# Este módulo quedará para futuras implementaciones donde se consulte información online.
-def obtener_informacion_artista(artista):
-    # Placeholder: Lógica para consultar una API externa y obtener información sobre el artista.
-    pass
